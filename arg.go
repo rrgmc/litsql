a2a5d21588f433031354c03ce1b8e9c4e7a5adbc
Rangel Reale <rangelspam@gmail.com>
2024-06-01 07:45:11 -0300

    comments

--- arg.go
+++ arg.go
@@ -5,20 +5,24 @@ import (
 	"fmt"
 )
 
+// Argument is the base interface for query arguments.
 type Argument interface {
 	isArgument()
 }
 
+// NamedArgument represents an argument were its value will be provided by name.
 type NamedArgument interface {
 	Argument
 	Name() string
 }
 
+// ValuedArgument represents an argument were its value will be provided by this instance.
 type ValuedArgument interface {
 	Argument
 	Value() (any, error)
 }
 
+// DBNamedArgument is like NamedArgument but its value will be wrapped using [sql.Named].
 type DBNamedArgument interface {
 	Argument
 	DBName() string
