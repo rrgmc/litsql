8ad234eae13b335c5534909414496954f5fde4c1
Rangel Reale <rangelspam@gmail.com>
2024-06-02 18:08:16 -0300

    test writer

---
+++ sq/writer_test.go
@@ -0,0 +1,64 @@
+package sq
+
+import (
+	"bytes"
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestNewWriter(t *testing.T) {
+	var buf bytes.Buffer
+
+	w := NewWriter(&buf, WithWriterUseNewLine(false))
+
+	w.AddSeparator(true)
+	w.Write("a")
+	w.WriteSeparator()
+	w.Write("b")
+	w.WriteNewLine()
+	w.Write("c")
+	w.AddSeparator(false)
+	w.Write("d")
+	w.AddSeparator(true)
+	w.Write("e")
+	w.StartQuery()
+	w.AddSeparator(false)
+	w.Write("f")
+	w.Indent()
+	w.Write("g")
+	w.Dedent()
+	w.Write("h")
+
+	assert.NilError(t, w.Err())
+	assert.Equal(t, "a bc d efgh", buf.String())
+}
+
+func TestNewWriterNewLine(t *testing.T) {
+	var buf bytes.Buffer
+
+	w := NewWriter(&buf,
+		WithWriterUseNewLine(true),
+		WithWriterIndentStr(" "))
+
+	w.AddSeparator(true)
+	w.Write("a")
+	w.WriteSeparator()
+	w.Write("b")
+	w.WriteNewLine()
+	w.Write("c")
+	w.AddSeparator(false)
+	w.Write("d")
+	w.AddSeparator(true)
+	w.Write("e")
+	w.StartQuery()
+	w.AddSeparator(false)
+	w.Write("f")
+	w.Indent()
+	w.Write("g")
+	w.Dedent()
+	w.Write("h")
+
+	assert.NilError(t, w.Err())
+	assert.Equal(t, "a\nb\nc d\nefgh", buf.String())
+}
