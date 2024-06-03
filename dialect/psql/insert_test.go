c2b8e48deb72c041eb642d5ae210738128205f68
Rangel Reale <rangelspam@gmail.com>
2024-06-03 15:25:21 -0300

    rename

--- dialect/psql/insert_test.go
+++ dialect/psql/insert_test.go
@@ -5,14 +5,13 @@ import (
 
 	"github.com/rrgmc/litsql/dialect/psql"
 	"github.com/rrgmc/litsql/dialect/psql/im"
-	"github.com/rrgmc/litsql/expr"
 	"gotest.tools/v3/assert"
 )
 
 func TestInsert(t *testing.T) {
 	query := psql.Insert(
 		im.Into("device", "id", "name"),
-		im.Values(expr.S("d1")),
+		im.ValuesS("d1"),
 	)
 	queryStr, params, err := query.Build()
 
