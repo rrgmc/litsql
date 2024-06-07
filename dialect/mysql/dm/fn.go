96b7b91e6b626f1e57c1d13fb9f9d0502730616c
Rangel Reale <rangelspam@gmail.com>
2024-06-06 22:17:01 -0300

    mysql

--- dialect/mysql/dm/fn.go
+++ dialect/mysql/dm/fn.go
@@ -19,7 +19,7 @@ func InnerJoin(table string) JoinChain {
 	return idm.InnerJoin[tag.DeleteTag](table)
 }
 
-func InnerJoinE(table litsql.Expression) JoinChain {
+func InnerJoinExpr(table litsql.Expression) JoinChain {
 	return idm.InnerJoinExpr[tag.DeleteTag](table)
 }
 
@@ -27,7 +27,7 @@ func LeftJoin(table string) JoinChain {
 	return idm.LeftJoin[tag.DeleteTag](table)
 }
 
-func LeftJoinE(table litsql.Expression) JoinChain {
+func LeftJoinExpr(table litsql.Expression) JoinChain {
 	return idm.LeftJoinExpr[tag.DeleteTag](table)
 }
 
@@ -35,7 +35,7 @@ func RightJoin(table string) JoinChain {
 	return idm.RightJoin[tag.DeleteTag](table)
 }
 
-func RightJoinE(table litsql.Expression) JoinChain {
+func RightJoinExpr(table litsql.Expression) JoinChain {
 	return idm.RightJoinExpr[tag.DeleteTag](table)
 }
 
@@ -43,7 +43,7 @@ func FullJoin(table string) JoinChain {
 	return idm.FullJoin[tag.DeleteTag](table)
 }
 
-func FullJoinE(table litsql.Expression) JoinChain {
+func FullJoinExpr(table litsql.Expression) JoinChain {
 	return idm.FullJoinExpr[tag.DeleteTag](table)
 }
 
@@ -51,7 +51,7 @@ func CrossJoin(table string) JoinChain {
 	return idm.CrossJoin[tag.DeleteTag](table)
 }
 
-func CrossJoinE(table litsql.Expression) JoinChain {
+func CrossJoinExpr(table litsql.Expression) JoinChain {
 	return idm.CrossJoinExpr[tag.DeleteTag](table)
 }
 
@@ -59,7 +59,7 @@ func StraightJoin(table string) JoinChain {
 	return idm.StraightJoin[tag.DeleteTag](table)
 }
 
-func StraightJoinE(table litsql.Expression) JoinChain {
+func StraightJoinExpr(table litsql.Expression) JoinChain {
 	return idm.StraightJoinExpr[tag.DeleteTag](table)
 }
 
@@ -75,11 +75,11 @@ func Using(table string) FromChain {
 	return idm.Using[tag.DeleteTag](table)
 }
 
-func UsingE(table litsql.Expression) FromChain {
+func UsingExpr(table litsql.Expression) FromChain {
 	return idm.UsingExpr[tag.DeleteTag](table)
 }
 
-func UsingQ(q mysql.SelectQuery) FromChain {
+func UsingQuery(q mysql.SelectQuery) FromChain {
 	return idm.UsingQuery[tag.DeleteTag, tag.SelectTag](q)
 }
 
@@ -87,11 +87,11 @@ func Where(condition string) mysql.DeleteMod {
 	return idm.Where[tag.DeleteTag](condition)
 }
 
-func WhereE(condition litsql.Expression) mysql.DeleteMod {
+func WhereExpr(condition litsql.Expression) mysql.DeleteMod {
 	return idm.WhereExpr[tag.DeleteTag](condition)
 }
 
-func WhereC(query string, args ...any) mysql.DeleteMod {
+func WhereClause(query string, args ...any) mysql.DeleteMod {
 	return idm.WhereClause[tag.DeleteTag](query, args...)
 }
 
@@ -99,6 +99,6 @@ func With(name string, columns ...string) WithChain {
 	return idm.With[tag.DeleteTag](name, columns...)
 }
 
-func WithE(name string, columns ...litsql.Expression) WithChain {
+func WithExpr(name string, columns ...litsql.Expression) WithChain {
 	return idm.WithExpr[tag.DeleteTag](name, columns...)
 }
