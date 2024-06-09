e99a99d83ab02fb964693e559fbeb509935ab158
Rangel Reale <rangelspam@gmail.com>
2024-06-09 07:52:11 -0300

    test

--- internal/codegen/dialectgen.go
+++ internal/codegen/dialectgen.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"path/filepath"
 	"runtime"
+	"slices"
 	"strings"
 )
 
@@ -18,15 +20,18 @@ func main() {
 }
 
 func run() error {
-
 	currentDir := getCurrentDir()
 	ismDir := filepath.Clean(filepath.Join(currentDir, "..", "ism"))
+	// dialectDir := filepath.Clean(filepath.Join(currentDir, "..", "..", "dialect"))
+	// psqSmlDir := filepath.Join(dialectDir, "psql")
 
 	fset := token.NewFileSet()
 	pkgs, err := parser.ParseDir(fset, ismDir, nil, parser.ParseComments|parser.SkipObjectResolution)
 	if err != nil {
 		return err
 	}
+
+	var funcs []*ast.FuncDecl
 
 	for _, pkg := range pkgs {
 		fmt.Printf("%s %s %s\n", strings.Repeat("=", 15), pkg.Name, strings.Repeat("=", 15))
@@ -39,15 +44,24 @@ func run() error {
 
 			for _, d := range file.Decls {
 				switch dt := d.(type) {
-				case *ast.GenDecl:
-					fmt.Printf("GenDecl\n")
+				// case *ast.GenDecl:
+				// 	fmt.Printf("GenDecl\n")
 				case *ast.FuncDecl:
-					fmt.Printf("Func: %s\n", dt.Name.Name)
+					// fmt.Printf("Func: %s\n", dt.Name.Name)
+					funcs = append(funcs, dt)
 				default:
 					fmt.Printf("Unknown type: %T\n", dt)
 				}
 			}
 		}
+	}
+
+	slices.SortFunc(funcs, func(a, b *ast.FuncDecl) int {
+		return cmp.Compare(a.Name.Name, b.Name.Name)
+	})
+
+	for _, funcDecl := range funcs {
+		fmt.Printf("Func: %s\n", funcDecl.Name.Name)
 	}
 
 	return nil
