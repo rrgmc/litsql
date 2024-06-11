636d28962613e1016619b469cd8d3d06a9787ae3
Rangel Reale <rangelspam@gmail.com>
2024-06-11 09:16:21 -0300

    chain

--- dialect/psql/sm/adapter.go
+++ dialect/psql/sm/adapter.go
@@ -1,16 +1,11 @@
 package sm
 
-import (
-	tag "github.com/rrgmc/litsql/dialect/psql/tag"
-	"github.com/rrgmc/litsql/internal/ichain"
-)
-
 // type groupByChainAdapter struct {
 // 	// sq.ModTagImpl[tag.SelectTag]
 // 	*ichain.GroupByChain[tag.SelectTag, GroupByChain]
 // }
 
-type groupByChainAdapter = ichain.GroupByChain[tag.SelectTag, GroupByChain]
+// type groupByChainAdapter = ichain.GroupByChain[tag.SelectTag, GroupByChain]
 
 // func (a *groupByChainAdapter) Apply(apply litsql.QueryBuilder) {
 // 	a.chain.Apply(apply)
