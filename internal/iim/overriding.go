4ab3903374ca2e3c9e96aa3a042151c5aba24a2f
Rangel Reale <rangelspam@gmail.com>
2024-06-11 07:39:39 -0300

    directives

--- internal/iim/overriding.go
+++ internal/iim/overriding.go
@@ -6,12 +6,14 @@ import (
 	"github.com/rrgmc/litsql/sq"
 )
 
+//litsql:dialects psql
 func OverridingSystem[T any]() sq.QueryMod[T] {
 	return sq.QueryModFunc[T](func(a litsql.QueryBuilder) {
 		a.AddQueryClause(&iclause.InsertOverriding{Overriding: "SYSTEM"})
 	})
 }
 
+//litsql:dialects psql
 func OverridingUser[T any]() sq.QueryMod[T] {
 	return sq.QueryModFunc[T](func(a litsql.QueryBuilder) {
 		a.AddQueryClause(&iclause.InsertOverriding{Overriding: "USER"})
